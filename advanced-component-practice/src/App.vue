94f5e99f42db4a20a2d1c74e23d66f136218e70b
Maureen Holland <maureen.holland11@gmail.com>
2018-10-24 16:52:50 -0400

    separate into components, pass quotes to grid and single quote template

--- advanced-component-practice/src/App.vue
+++ advanced-component-practice/src/App.vue
@@ -2,15 +2,43 @@
     <div class="container">
         <div class="row">
             <div class="col-xs-12">
-               
+            	<button @click="selectedComponent = 'app-quote'">Quote</button>
+            	<button @click="selectedComponent = 'app-author'">Author</button>
+            	<button @click="selectedComponent = 'app-new'">New</button>
+            	
+            	<hr>
+            	<keep-alive>
+            		<component :is="selectedComponent">
+            			<p>Default Content</p>
+            		</component>
+            	</keep-alive>	
+               <!-- <app-quote> -->
+               		<!-- <h2 slot="title">{{ quoteTitle }}</h2> -->
+               		<!-- <p slot="subtitle">Actual subtitle</p> -->
+               		<!-- <p slot="content">A wonderful quote</p> -->
+               <!-- </app-quote> -->
             </div>
         </div>
     </div>
 </template>
 
 <script>
+	import Quote from './components/Quote.vue';
+	import Author from './components/Author.vue';
+	import New from './components/New.vue';
 
     export default {
+    	data:function(){
+			return {
+				quoteTitle: 'The Quote',
+				selectedComponent: 'app-quote'
+			}
+		},
+    	components: {
+    		'app-quote': Quote,
+    		'app-author': Author,
+    		'app-new': New
+    	}
         
     }
 </script>
--- quotes/src/App.vue
+++ quotes/src/App.vue
@@ -1,12 +1,27 @@
 <template>
     <div class="container">
-    
+    	<app-grid :quotes="quotes"></app-grid>
     </div>
 </template>
 
 <script>
+	import Grid from './components/QuoteGrid.vue';
+
     export default {
-        
+        data: function(){
+        	return {
+        		quotes: [
+        			'Just a placeholder',
+        			'And another',
+        			'One more',
+        			'How\'s the grid going?'
+        		],
+        		maxQuotes: 10
+        	}
+        },
+        components: {
+        	'app-grid': Grid
+        }
     }
 </script>
 
